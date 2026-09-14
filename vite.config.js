883d8e44221c57cb7f15988bab36bae68179f678
Casavellumine <felipe.casavellumine@gmail.com>
2026-09-14 19:52:32 -0300

    Configura plugin cloudflare() no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
-// Configuração do Vite para build estático
 export default defineConfig({
+  plugins: [cloudflare()],
   build: {
     outDir: 'dist',
   },
